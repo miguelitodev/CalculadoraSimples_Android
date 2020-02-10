6bd1c8d2f771d2b37c039e8f2b1564b66b9a8daa
miguelrisquelme <miguelnascimento22@hotmail.com>
2020-02-10 18:58:12 -0300

    Tela splash | Icone do app | Nome do app

--- app/app/src/main/java/com/miguelrisquelme/calculadora_simples/MainActivity.java
+++ app/app/src/main/java/com/miguelrisquelme/calculadora_simples/MainActivity.java
@@ -3,6 +3,9 @@ package com.miguelrisquelme.calculadora_simples;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,58 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void Somar(View v){
+        EditText txtPrimeiroValor = findViewById(R.id.txtPrimeiroValor);
+        EditText txtSegundoValor = findViewById(R.id.txtSegundoValor);
+        TextView txtResultado = findViewById(R.id.txtResultado);
+
+        float valor1 = Float.parseFloat(txtPrimeiroValor.getText().toString());
+        float valor2 = Float.parseFloat(txtSegundoValor.getText().toString());
+
+        float resultado = valor1 + valor2;
+
+        txtResultado.setText(String.valueOf(resultado));
+    }
+
+    public void Subtrair(View v){
+        EditText txtPrimeiroValor = findViewById(R.id.txtPrimeiroValor);
+        EditText txtSegundoValor = findViewById(R.id.txtSegundoValor);
+        TextView txtResultado = findViewById(R.id.txtResultado);
+
+        float valor1 = Float.parseFloat(txtPrimeiroValor.getText().toString());
+        float valor2 = Float.parseFloat(txtSegundoValor.getText().toString());
+
+        float resultado = valor1 - valor2;
+
+        txtResultado.setText(String.valueOf(resultado));
+    }
+
+    public void Dividir(View v){
+        EditText txtPrimeiroValor = findViewById(R.id.txtPrimeiroValor);
+        EditText txtSegundoValor =  findViewById(R.id.txtSegundoValor);
+        TextView txtResultado = findViewById(R.id.txtResultado);
+
+        float valor1 = Float.parseFloat(txtPrimeiroValor.getText().toString());
+        float valor2 = Float.parseFloat(txtSegundoValor.getText().toString());
+
+        float resultado = valor1 / valor2;
+
+        txtResultado.setText(String.valueOf(resultado));
+    }
+
+    public void Multiplicar(View v){
+        EditText txtPrimeiroValor = findViewById(R.id.txtPrimeiroValor);
+        EditText txtSegundoValor = findViewById(R.id.txtSegundoValor);
+        TextView txtResultado = findViewById(R.id.txtResultado);
+
+        float valor1 = Float.parseFloat(txtPrimeiroValor.getText().toString());
+        float valor2 = Float.parseFloat(txtSegundoValor.getText().toString());
+
+        float resultado = valor1 * valor2;
+
+        txtResultado.setText(String.valueOf(resultado));
+    }
+
 }
+
